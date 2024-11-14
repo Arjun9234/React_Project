5857e4ff2615824c797bae53ed4306a5a5064870
Arjun Arora <arjunarora3682@gmail.com>
2024-11-14 21:26:23 +0530

    Update Popular.jsx

--- Frontend/src/Components/Popular/Popular.jsx
+++ Frontend/src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ const Popular = () => {
  const [popularProducts,setPopularProducts]= useState([]);
 
  useEffect(()=>{
-  fetch('http://localhost:4000/popularinwomen')
+  fetch('https://react-project-backend-beld.onrender.com/popularinwomen')
   .then((response)=>response.json())
   .then((data)=>setPopularProducts(data));
  },[])
